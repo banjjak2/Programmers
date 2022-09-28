154f536d59ab5c8fd708585698f4d6a4c74cb8e8
wooki <wooki.jang@groundx.xyz>
2022-09-28 20:39:51 +0900

    JadenCase 문자열 만들기
    https://school.programmers.co.kr/learn/courses/30/lessons/12951

---
+++ src/main/kotlin/P12951.kt
@@ -0,0 +1,26 @@
+class P12951 {
+    class Solution {
+        fun solution(s: String): String {
+            val sb = StringBuilder()
+            for (i in s.indices) {
+                val chr = s[i]
+                if (chr == ' ') sb.append(' ')
+                else if (chr in 'A'..'Z') {
+                    if (i > 0 && s[i-1] != ' ') sb.append(chr + 0x20)
+                    else sb.append(chr)
+                } else if (chr in 'a' .. 'z') {
+                    if (i == 0 || s[i-1] == ' ') sb.append(chr - 0x20)
+                    else sb.append(chr)
+                } else if (chr in '0' .. '9') sb.append(chr)
+            }
+
+            return sb.toString()
+        }
+    }
+}
+
+fun main() {
+    val sol = P12951.Solution()
+    val s = "3people unFollowed me"
+    println(sol.solution(s))
+}
