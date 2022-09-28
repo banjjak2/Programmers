dfcb89a259c111f7271356e11d144f3a290af43c
wooki <wooki.jang@groundx.xyz>
2022-09-28 21:13:13 +0900

    최솟값 만들기
    https://school.programmers.co.kr/learn/courses/30/lessons/12941

---
+++ src/main/kotlin/P12941.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class P12941 {
+    static class Solution
+    {
+        public int solution(int []A, int []B)
+        {
+            Arrays.sort(A);
+            Arrays.sort(B);
+            int answer = 0;
+            for (int i=0; i<A.length; i++) {
+                answer += (A[i] * B[A.length - i - 1]);
+            }
+
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new P12941.Solution();
+        int[] A = {1, 4, 2};
+        int[] B = {5, 4, 4};
+        solution.solution(A, B);
+    }
+}
