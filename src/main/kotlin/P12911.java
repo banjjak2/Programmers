5822c7ddc8a73304a0b1625a0cd5d3821c80d0a0
banjjak2 <interr0bang@naver.com>
2022-09-30 11:43:14 +0900

    다음 큰 숫자
    https://school.programmers.co.kr/learn/courses/30/lessons/12911

---
+++ src/main/kotlin/P12911.java
@@ -0,0 +1,19 @@
+public class P12911 {
+    static class Solution {
+        public int solution(int n) {
+            int oneCount = Integer.bitCount(n);
+            int i = n + 1;
+            while (oneCount != Integer.bitCount(i)) {
+                i++;
+            }
+
+            return i;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new P12911.Solution();
+        int n = 15;
+        System.out.println(sol.solution(n));
+    }
+}
