1b52ff9f62b29358d92193cb8ff8d6b6a63561f5
wooki <wooki.jang@groundx.xyz>
2022-09-30 11:05:32 +0900

    올바른 괄호
    https://school.programmers.co.kr/learn/courses/30/lessons/12909

---
+++ src/main/kotlin/P12909.java
@@ -0,0 +1,25 @@
+public class P12909 {
+    static class Solution {
+        boolean solution(String s) {
+            int open = 0;
+            for (int i=0; i<s.length(); i++) {
+                char chr = s.charAt(i);
+                if (chr == '(') {
+                    open++;
+                } else {
+                    if (i == 0 || open <= 0) return false;
+                    open--;
+                }
+            }
+
+            if (open != 0) return false;
+            else return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new P12909.Solution();
+        String s = "(()))(";
+        System.out.println(sol.solution(s));
+    }
+}
