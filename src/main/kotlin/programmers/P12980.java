97ded2db8610e88c84ae931b732b63a8844f4c45
wooki <interr0bang@naver.com>
2022-10-15 17:09:40 +0900

    Update P12980.java

--- src/main/kotlin/programmers/P12980.java
+++ src/main/kotlin/programmers/P12980.java
@@ -8,14 +8,14 @@ package programmers;
  *                      2진수
  * n = 1, J             (1)
  * n = 2, J T           (10)
- * n = 3, J W J         (011)
- * n = 4, J W W         (100)
- * n = 5, J W W J       (101)
- * n = 6, J W J W       (110)
- * n = 7, J W J W J     (111)
- * n = 8, J W W W       (1000)
- * n = 9, J W W W J     (1001)
- * n = 10, J W W J W    (1010)
+ * n = 3, J T J         (011)
+ * n = 4, J T T         (100)
+ * n = 5, J T T J       (101)
+ * n = 6, J T J T       (110)
+ * n = 7, J T J T J     (111)
+ * n = 8, J T T T       (1000)
+ * n = 9, J T T T J     (1001)
+ * n = 10, J T T J T    (1010)
  * ...
  *
  * 결국 J의 개수와 2진수 1의 개수가 같음
